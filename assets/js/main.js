0b34335f0d703de2c3685897b36268bfda87a78f
Alexey Stalpetc <stalpetc@gmail.com>
2024-12-30 15:36:54 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
 	var	$window = $(window),
 		$body = $('body'),
 		$wrapper = $('#page-wrapper'),
-		$banner = $('#banner'),
+		$banner1 = $('#banner1'),
 		$header = $('#header');
 
 	// Breakpoints.
@@ -66,12 +66,12 @@
 			});
 
 	// Header.
-		if ($banner.length > 0
+		if ($banner1.length > 0
 		&&	$header.hasClass('alt')) {
 
 			$window.on('resize', function() { $window.trigger('scroll'); });
 
-			$banner.scrollex({
+			$banner1.scrollex({
 				bottom:		$header.outerHeight() + 1,
 				terminate:	function() { $header.removeClass('alt'); },
 				enter:		function() { $header.addClass('alt'); },
